cb1fe54eaf7725a284f25bb0e37d85d670a83571
João Caseiro <joao.caseiro@gmail.com>
2025-03-06 23:19:57 -0300

    Correção do relacionamento da amostragem das tabelas no insert -v3.

--- insert_table.py
+++ insert_table.py
@@ -127,17 +127,23 @@ def display_table_samples(spark: SparkSession, database_name: str, tables: list)
         tables (list): A list of tables of the database.
     """
     logger.info("Displaying sample rows from tables")
-    transacoes_ids = []
+    
+    transacoes_sample_rows = None
+    clientes_sample_rows = None
 
     for table_name in tables:
         sample_rows = spark.sql(f"SELECT * FROM {database_name}.{table_name} LIMIT 3").collect()
         logger.info(f"Sample rows from table '{table_name}':")
         for row in sample_rows:
             logger.info(str(row))
+        
         if table_name == 'transacoes_cartao':
-            transacoes_ids = [row.id_usuario for row in sample_rows]
+            transacoes_sample_rows = sample_rows
+        elif table_name == 'clientes':
+            clientes_sample_rows = sample_rows
 
-    if 'transacoes_cartao' in tables and 'clientes' in tables:
+    if transacoes_sample_rows and clientes_sample_rows:
+        transacoes_ids = [row.id_usuario for row in transacoes_sample_rows]
         logger.info(f"Sampled id_usuario from 'transacoes_cartao' table: {transacoes_ids}")
 
         clientes_sample = spark.sql(f"""
