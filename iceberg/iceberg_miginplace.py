9b0ff3349109c17ef19dd7a7856bf26577d90225
João Caseiro <joao.caseiro@gmail.com>
2025-03-11 19:30:53 -0300

    Correção Comparação de contagem de linhas - v1.

--- iceberg/iceberg_miginplace.py
+++ iceberg/iceberg_miginplace.py
@@ -141,12 +141,12 @@ def iceberg_sanity_checks(logger: logging.Logger, spark: SparkSession, database_
         logger.info(f"Count query 1: {count_query1}")
         count_query2 = f"SELECT COUNT(*) as count FROM {database_name}.{table_name}"
         logger.info(f"Count query 2: {count_query2}")
-        count1, count2, counts_match = compare_query_results(spark, count_query1, count_query2, "Row counts")
-        logger.info(f"Count query results match: {counts_match}")
-        logger.info(f"Counts Iceberg Snapshot Table: {count1}")
-        logger.info(f"Counts Original Table: {count2}")
+        count1, count2, counts_match = compare_query_results(logger, spark, count_query1, count_query2, "Row counts")
+        logger.debug(f"Count query results match: {counts_match}")
+        logger.debug(f"Counts Iceberg Snapshot Table: {count1}")
+        logger.debug(f"Counts Original Table: {count2}")
         checks_passed = checks_passed and counts_match
-        logger.info(f"Checks passed: {checks_passed}")
+        logger.debug(f"Checks passed: {checks_passed}")
         
         logger.info(f"Iceberg snapshot table row count: {count1[0]['count']}")
         logger.info(f"Original table row count: {count2[0]['count']}\n")
