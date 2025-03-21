b38ca3b991136aacdda6e3d3b7b165339a34a764
João Caseiro <joao.caseiro@gmail.com>
2025-03-21 11:53:46 -0300

    Atualização do script airflow

--- cde/bank-mesh-job-airflow.py
+++ cde/bank-mesh-job-airflow.py
@@ -1,12 +1,11 @@
 from airflow import DAG
 from airflow.utils import timezone
 from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
-from datetime import timedelta
 from dateutil import parser
 
 dag = DAG(
-    dag_id='job_mesh_c1fb3a13',
-    start_date=parser.isoparse('2025-03-12T20:16:59Z').replace(tzinfo=timezone.utc),
+    dag_id='job_mesh',
+    start_date=parser.isoparse('2025-03-21T14:50:45Z').replace(tzinfo=timezone.utc),
     schedule_interval=None,
     is_paused_upon_creation=False,
     default_args={
@@ -14,63 +13,45 @@ dag = DAG(
     },
 )
 
-Tables_creation = CDEJobRunOperator(
+Create_Tables = CDEJobRunOperator(
     job_name='job-100',
-    depends_on_past=True,
-    trigger_rule='all_success',
-    task_id='Tables_creation',
+    task_id='Create_Tables',
     dag=dag,
 )
 
-New_ingestion = CDEJobRunOperator(
+Partition_and_Bucketing_Ingestion = CDEJobRunOperator(
     job_name='job-101',
     depends_on_past=True,
     trigger_rule='all_success',
-    task_id='New_ingestion',
+    task_id='Partition_and_Bucketing_Ingestion',
     dag=dag,
 )
 
-Second_ingestion = CDEJobRunOperator(
-    job_name='job-101',
-    depends_on_past=True,
-    trigger_rule='all_success',
-    task_id='Second_ingestion',
-    dag=dag,
-)
-
-Running_simple_queries = CDEJobRunOperator(
+Simple_Queries = CDEJobRunOperator(
     job_name='job-102',
     depends_on_past=True,
     trigger_rule='all_success',
-    task_id='Running_simple_queries',
+    task_id='Simple_Queries',
     dag=dag,
 )
 
-Running_complex_queries = CDEJobRunOperator(
+Complex_Queries = CDEJobRunOperator(
     job_name='job-103',
     depends_on_past=True,
     trigger_rule='all_success',
-    task_id='Running_complex_queries',
+    task_id='Complex_Queries',
     dag=dag,
 )
 
-In_place_Iceberg_migration = CDEJobRunOperator(
+Iceberg_Migration_InPlace = CDEJobRunOperator(
     job_name='job-104',
     depends_on_past=True,
     trigger_rule='all_success',
-    task_id='In-place_Iceberg_migration',
+    task_id='Iceberg_Migration_InPlace',
     dag=dag,
 )
 
-Cleaning = CDEJobRunOperator(
-    job_name='job-clean',
-    task_id='Cleaning',
-    dag=dag,
-)
-
-Tables_creation << [Cleaning]
-New_ingestion << [Tables_creation]
-Second_ingestion << [New_ingestion]
-Running_simple_queries << [Second_ingestion]
-Running_complex_queries << [Second_ingestion]
-In_place_Iceberg_migration << [Running_simple_queries, Running_complex_queries]
+Partition_and_Bucketing_Ingestion << [Create_Tables]
+Simple_Queries << [Partition_and_Bucketing_Ingestion]
+Complex_Queries << [Partition_and_Bucketing_Ingestion]
+Iceberg_Migration_InPlace << [Simple_Queries, Complex_Queries]
