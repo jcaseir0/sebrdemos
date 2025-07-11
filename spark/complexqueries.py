455d001dfa426f56e2ee5d9721b6e54362487c5e
clouderajguimaraes <jguimaraes@cloudera.com>
2025-07-11 10:25:02 -0300

    Update complexqueries.py

--- spark/complexqueries.py
+++ spark/complexqueries.py
@@ -107,7 +107,6 @@ def correlacao_limite_gastos():
 print(f"\nNúmero de linhas da tabela clientes: {num_clientes}")
 num_clientes.show(10)
 print(f"\nNúmero de linhas da tabela transacoes_cartao: {num_transacoes}")
-num_transacoes.show(10)
 
 logger.info("\nExecuting financial analysis queries\n")
 
